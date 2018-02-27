2be89c4e2b989e494124cbc5b9d95354f9afbc9d
nazaninreihani <nazanin.reyh@gmail.com>
2018-02-27 09:41:13 +0330

    refactor

--- src/javascript/app_2/pages/trading/components/elements/sidebar.jsx
+++ src/javascript/app_2/pages/trading/components/elements/sidebar.jsx
@@ -3,32 +3,40 @@ import React from 'react';
 class Sidebar extends React.PureComponent {
     constructor(props) {
         super(props);
+        const defaultSelectedId = props.items ? (props.items[0].submenu ? props.items[0].submenu[0].id : props.items[0].id) : '';
+        const defaultOpenTargetId = props.items ? props.items[0].id : '';
+
         this.state = {
-            selectedId: props.items[0].submenu ? props.items[0].submenu[0].id : props.items[0].id,
-            isOpen: props.items[0].id,
+            selectedId: defaultSelectedId,
+            openTargetId: defaultOpenTargetId,
         };
     }
 
-    onClick = (e: SyntheticEvent) => {
+    onClick = e => {
         if (/submenu-item/.test(e.target.className)) {
             this.setState({ selectedId: e.target.id });
         } else {
-            this.setState({ selectedId: e.target.id, isOpen: e.target.id });
+            this.setState({ selectedId: e.target.id, openTargetId: e.target.id });
         }
     }
 
-    openSubmenu = (e: SyntheticEvent) => {
-        if (this.state.isOpen !== e.target.id) {
+    openSubmenu = e => {
+        if (this.state.openTargetId !== e.target.id) {
             const firstListItem = this.props.items.filter(it => it.id == e.target.id);
-            this.setState({ isOpen: e.target.id, selectedId: firstListItem[0].submenu[0].id });
+            this.setState({ openTargetId: e.target.id, selectedId: firstListItem[0].submenu[0].id });
         }
     }
 
     render() {
-        const { selectedId, isOpen } = this.state;
+        const { selectedId, openTargetId } = this.state;
         return (
           <div className="sidebar-wrapper" id={this.props.id}>
-              <SidebarSubmenu items={this.props.items} onClick={this.onClick} selectedId={selectedId} openSubmenu={this.openSubmenu} isOpen={isOpen} />
+              <SidebarSubmenu
+                items={this.props.items}
+                onClick={this.onClick}
+                selectedId={selectedId}
+                openSubmenu={this.openSubmenu}
+                openTargetId={openTargetId} />
               <SidebarContent content={this.props.content} selectedId={selectedId} />
           </div>
         )
@@ -43,7 +51,7 @@ class SidebarContent extends React.PureComponent {
     render() {
         return (
           <div className='sidebar-collapsible-content'>
-              {this.props.content.map((item, idx) => (
+              {this.props.content && this.props.content.map((item, idx) => (
                 <div key={idx} id={item.id} className={this.props.selectedId == item.id ? 'visible' : 'invisible'}>
                     {item.cont}
                 </div>
@@ -60,27 +68,32 @@ class SidebarSubmenu extends React.PureComponent {
 
     render() {
         const getHref = item_id => `#${item_id}`;
-        const { isOpen } = this.props;
+        const { openTargetId } = this.props;
         return (
             <div className="sidebar-submenu">
                 <ul>
-                    {this.props.items.map((item, idx) => (
-                      <li key={idx} className={(item.submenu ? 'has-submenu' : '') + (item.id == this.props.selectedId ? 'selected' : '')}>
+                    {this.props.items && this.props.items.map((item, idx) => (
+                      <li
+                        key={idx}
+                        className={(item.submenu ? 'has-submenu' : '') + (item.id == this.props.selectedId ? 'active' : '')}>
                           { item.submenu &&
                           <React.Fragment>
-                              <a id={item.id}
-                                 className={isOpen == item.id ? 'selected' : ''}
-                                 href={getHref(item.id)}
-                                 onClick={this.props.openSubmenu}>
+                              <a
+                                id={item.id}
+                                className={openTargetId == item.id ? 'selected' : ''}
+                                href={getHref(item.id)}
+                                onClick={this.props.openSubmenu}>
                                   {item.text}
-                                  </a>
-                              <ul className={isOpen == item.id ? 'collapsed' : 'compressed'}>
+                              </a>
+                              <ul className={openTargetId == item.id ? 'collapsed' : 'compressed'}>
                                   { item.submenu.map((submenu, idx_submenu) => (
                                     <li key={idx_submenu}
                                         className={submenu.id == this.props.selectedId ? 'active' : ''}>
-                                        <a id={submenu.id} onClick={this.props.onClick}
-                                           className={'submenu-item ' + (submenu.id == this.props.selectedId ? 'selected' : '')}
-                                           href={getHref(submenu.id)}>
+                                        <a
+                                          id={submenu.id}
+                                          onClick={this.props.onClick}
+                                          className={'submenu-item ' + (submenu.id == this.props.selectedId ? 'selected' : '')}
+                                          href={getHref(submenu.id)}>
                                             {submenu.text}
                                         </a>
                                     </li>
@@ -88,11 +101,13 @@ class SidebarSubmenu extends React.PureComponent {
                               </ul>
                           </React.Fragment>}
                           { !item.submenu && item.text &&
-                          <a
-                            className={item.id == this.props.selectedId ? 'selected' : ''}
-                            id={item.id}
-                            onClick={this.props.onClick}
-                            href={getHref(item.id)}>{item.text}</a>}
+                            <a
+                              className={item.id == this.props.selectedId ? 'selected' : ''}
+                              id={item.id}
+                              onClick={this.props.onClick}
+                              href={getHref(item.id)}>{item.text}
+                            </a>
+                          }
                       </li>
                     ))}
                 </ul>
@@ -114,7 +129,7 @@ const dummyitems = [
     { id: 'datafeed',  text: 'Data Feed Section' },
 ];
 
-const contents = [
+/*const contents = [
   {
     id: 'copyright',
     cont: <div>copyright</div>
@@ -132,7 +147,6 @@ const contents = [
 Sidebar.defaultProps = {
     items: dummyitems,
     content: contents,
-}
-
+}*/
 
 export default Sidebar;
